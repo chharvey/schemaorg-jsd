e82f42c4ef46bbf84e1bc860158a9ef7db26191a
Chris Harvey <chrisharvey2pi@gmail.com>
2018-01-05 16:48:52 -0500

    add validation gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,27 @@
+const fs   = require('fs')
+const path = require('path')
+
 const gulp  = require('gulp')
 const jsdoc = require('gulp-jsdoc3')
 
+gulp.task('validate', function () {
+  const Ajv = require('ajv')
+  return fs.readdir('./schema/', 'utf8', function (err, files) {
+    if (err) throw err
+    files.forEach(function (file) {
+      fs.readFile(path.join(__dirname, './schema/', file), 'utf8', function (err, data) {
+        let ajv = new Ajv()
+        let is_schema_valid = ajv.validateSchema(JSON.parse(data))
+        if (!is_schema_valid) {
+          console.error(`Invalid schema! ${file}`)
+          console.error(ajv.errors)
+        } else {
+          console.log(`Valid schema: ${file}`)
+        }
+      })
+    })
+  })
+})
 
 // HOW-TO: https://github.com/mlucool/gulp-jsdoc3#usage
 gulp.task('docs:api', function () {
