ea99d537eccda78bb4e6aa75b15a4d163d4ad0e9
Chris Harvey <chrisharvey2pi@gmail.com>
2018-01-08 10:53:33 -0500

    update function params

--- index.js
+++ index.js
@@ -5,23 +5,14 @@ const Ajv = require('ajv')
 
 /**
  * Validate a JSON document against a Schema.org JSON schema file.
- * @param   {string} filepath the path of the JSON document to test; should be a `.json` or `.jsonld` file
+ * @param   {(!Object|string)} document the JSON or JSON-LD object to test, or its path pointing to a `.json` or `.jsonld` file
  * @param   {string} type the name of the Type to test against; should be a Class in http://schema.org/
  *                        ; see {@link ./docs/api/} for supported Types
- * @returns {boolean} `true` if the document passes validation, `false` otherwise
+ * @returns {boolean} `true` if the document passes validation, `false` if it fails
  */
-function sdoValidate(filepath, type) {
-  let schema, doc;
-  try {
-    schema = require(`./schema/${type}.jsd`)
-  } catch (e) {
-    schema = JSON.parse(fs.readFileSync(path.join(__dirname, `./schema/${type}.jsd`), 'utf8'))
-  }
-  try {
-    doc = require(filepath)
-  } catch (e) {
-    doc = JSON.parse(fs.readFileSync(filepath, 'utf8'))
-  }
+function sdoValidate(document, type) {
+  let schema = JSON.parse(fs.readFileSync(path.join(__dirname, `./schema/${type}.jsd`), 'utf8'))
+  let doc = (typeof document === 'string') ? JSON.parse(fs.readFileSync(document, 'utf8')) : document
   let ajv = new Ajv()
   let is_data_valid = ajv.validate(schema, doc)
   if (!is_data_valid) {
