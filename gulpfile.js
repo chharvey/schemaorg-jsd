862c04c9a71468f070de25533031ffb3defbe59d
Chris Harvey <chrisharvey2pi@gmail.com>
2018-01-11 13:44:30 -0500

    simplify gulp validation task

--- gulpfile.js
+++ gulpfile.js
@@ -5,64 +5,9 @@ const gulp  = require('gulp')
 const jsdoc = require('gulp-jsdoc3')
 const Ajv   = require('ajv')
 
+const validateSchemata = require('./lib/validateSchemata.js')
 
-gulp.task('validate', function (callback) {
-  fs.readdir('./schema/', 'utf8', function (err, files) {
-    if (err) return callback.call(null, err)
-    let count = valid = 0
-    files.forEach(function (file) {
-      count++
-      fs.readFile(path.join(__dirname, './schema/', file), 'utf8', function (err, data) {
-        try { JSON.parse(data) } catch (e) {
-          e.filename = file
-          callback.call(null, e)
-          console.error(e)
-          return
-        }
-        let ajv = new Ajv()
-        let is_schema_valid = ajv.validateSchema(JSON.parse(data))
-        if (!is_schema_valid) {
-          let e = new TypeError(ajv.errors[0].message)
-          e.filename = file
-          e.details = ajv.errors[0]
-          callback.call(null, e)
-          console.error(e)
-        } else {
-          console.log(`Valid schema: ${file}`)
-          valid++
-        }
-      })
-    })
-    return callback.call(null)
-  })
-})
-
-
-gulp.task('validateSync', function () {
-  let files = fs.readdirSync('./schema/', 'utf8')
-  let count = valid = 0
-  files.forEach(function (file) {
-    count++
-    let data = fs.readFileSync(path.join(__dirname, './schema/', file), 'utf8')
-    try { JSON.parse(data) } catch (e) {
-      e.filename = file
-      console.error(e)
-      return
-    }
-    let ajv = new Ajv()
-    let is_schema_valid = ajv.validateSchema(JSON.parse(data))
-    if (!is_schema_valid) {
-      let e = new TypeError(ajv.errors[0].message)
-      e.filename = file
-      e.details = ajv.errors[0]
-      console.error(e)
-    } else {
-      console.log(`Valid schema: ${file}`)
-      valid++
-    }
-  })
-  return console.info(`Total valid files: ${valid} out of ${count}.`)
-})
+gulp.task('validate', validateSchemata)
 
 
 // HOW-TO: https://github.com/mlucool/gulp-jsdoc3#usage
@@ -72,4 +17,4 @@ gulp.task('docs:api', function () {
 })
 
 
-gulp.task('build', ['validateSync', 'docs:api'])
+gulp.task('build', ['validate', 'docs:api'])
