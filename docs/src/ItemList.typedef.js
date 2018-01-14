3d0e563439ee442a3672587a867b3770f1d41a4c
Chris Harvey <chrisharvey2pi@gmail.com>
2018-01-14 01:01:49 -0500

    simplify array-like properties

--- docs/src/ItemList.typedef.js
+++ docs/src/ItemList.typedef.js
@@ -2,6 +2,6 @@
  * @summary A list of items of any sort.
  * @see http://schema.org/ItemList
  * @typedef {Thing} ItemList
- * @property {(string|Thing|ListItem)=} itemListElement For itemListElement values, you can use simple strings (e.g. "Peter", "Paul", "Mary"), existing entities, or use ListItem.
+ * @property {(string|Thing|ListItem|Array<(string|Thing|ListItem)>)=} itemListElement For itemListElement values, you can use simple strings (e.g. "Peter", "Paul", "Mary"), existing entities, or use ListItem.
  * @property {number=} numberOfItems The number of items in an ItemList. Note that some descriptions might not fully describe all items in a list (e.g., multi-page pagination); in such cases, the numberOfItems would be for the entire list.
  */
