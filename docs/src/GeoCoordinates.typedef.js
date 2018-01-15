aa3378e5d77a46ef75d3d0581db0402243930c01
Chris Harvey <chrisharvey2pi@gmail.com>
2018-01-15 00:11:51 -0500

    add Place#geo and GeoCoordinates

---
+++ docs/src/GeoCoordinates.typedef.js
@@ -0,0 +1,8 @@
+/**
+ * @summary Entities that have a somewhat fixed, physical extension.
+ * @see http://schema.org/GeoCoordinates
+ * @typedef {StructuredValue} GeoCoordinates
+ * @property {(string|number)=} elevation The elevation of a location (WGS 84).
+ * @property {(string|number)=} latitude The latitude of a location (WGS 84).
+ * @property {(string|number)=} longitude The longitude of a location (WGS 84).
+ */
