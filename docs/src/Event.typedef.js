7bf06e24db3511aa6f2740107f3191b23847ad89
Chris Harvey <chrisharvey2pi@gmail.com>
2018-01-17 15:05:12 -0500

    add Event.jsd

---
+++ docs/src/Event.typedef.js
@@ -0,0 +1,8 @@
+/**
+ * @summary An event happening at a certain time and location, such as a concert, lecture, or festival.
+ * @see http://schema.org/Event
+ * @typedef {Thing} Event
+ * @property {string=} endDate The end date and time of the item (in ISO 8601 date format).
+ * @property {(string|Place|PostalAddress)=} location The location of for example where the event is happening, an organization is located, or where an action takes place.
+ * @property {string=} startDate The start date and time of the item (in ISO 8601 date format).
+ */
