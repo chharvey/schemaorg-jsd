4b331ffe62b61ef1ca8c6a5ba9b019f203e9a73b
Chris Harvey <chrisharvey2pi@gmail.com>
2018-01-17 15:09:08 -0500

    make {Organization,Event}#location array

--- docs/src/Event.typedef.js
+++ docs/src/Event.typedef.js
@@ -3,6 +3,6 @@
  * @see http://schema.org/Event
  * @typedef {Thing} Event
  * @property {string=} endDate The end date and time of the item (in ISO 8601 date format).
- * @property {(string|Place|PostalAddress)=} location The location of for example where the event is happening, an organization is located, or where an action takes place.
+ * @property {(string|Place|PostalAddress|Array<(string|Place|PostalAddress)>)=} location The location of for example where the event is happening, an organization is located, or where an action takes place.
  * @property {string=} startDate The start date and time of the item (in ISO 8601 date format).
  */
--- docs/src/Organization.typedef.js
+++ docs/src/Organization.typedef.js
@@ -5,6 +5,6 @@
  * @property {(string|PostalAddress)=} address Physical address of the item.
  * @property {(ContactPoint|Array<ContactPoint>)=} contactPoint A contact point for a person or organization.
  * @property {string=} email Email address.
- * @property {(string|Place|PostalAddress)=} location The location of for example where the event is happening, an organization is located, or where an action takes place.
+ * @property {(string|Place|PostalAddress|Array<(string|Place|PostalAddress)>)=} location The location of for example where the event is happening, an organization is located, or where an action takes place.
  * @property {string=} telephone The telephone number.
  */
