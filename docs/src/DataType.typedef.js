dca7b318f80fdee519657e04af50353a373aa881
Chris Harvey <chrisharvey2pi@gmail.com>
2018-01-20 15:06:33 -0500

    new file:   {Date,DateTime,Time,URL}.jsd, DataType.typedef

---
+++ docs/src/DataType.typedef.js
@@ -0,0 +1,23 @@
+/**
+ * @summary A date value in ISO 8601 date format.
+ * @see http://schema.org/Date
+ * @typedef {string} Date
+ */
+
+/**
+ * @summary A combination of date and time of day in the form [-]CCYY-MM-DDThh:mm:ss[Z|(+|-)hh:mm] (see Chapter 5.4 of ISO 8601).
+ * @see http://schema.org/DateTime
+ * @typedef {string} DateTime
+ */
+
+/**
+ * @summary A point in time recurring on multiple days in the form hh:mm:ss[Z|(+|-)hh:mm] (see XML schema for details).
+ * @see http://schema.org/Time
+ * @typedef {string} Time
+ */
+
+/**
+ * @summary Data type: URL.
+ * @see http://schema.org/URL
+ * @typedef {string} URL
+ */
