c9ff49ad776bd6c8af5d0df11495b8c285a321f4
Chris Harvey <chrisharvey2pi@gmail.com>
2018-01-22 15:08:05 -0500

    create Product.jsd

---
+++ docs/src/Product.typedef.js
@@ -0,0 +1,13 @@
+/**
+ * @summary Any offered product or service.
+ * @see http://schema.org/Product
+ * @typedef {Thing} Product
+ * @property {PropertyValue=} additionalProperty A property-value pair representing an additional characteristics of the entitity, e.g. a product feature or another characteristic for which there is no matching property in schema.org.
+ * @property {string=} award An award won by or for this item.
+ * @property {Organization=} brand The brand(s) associated with a product or service, or the brand(s) maintained by an organization or business person.
+ * @property {string=} color The color of the product.
+ * @property {(URL|ImageObject)=} logo An associated logo.
+ * @property {Organization=} manufacturer The manufacturer of the product.
+ * @property {string=} productID The product identifier, such as ISBN.
+ * @property {string=} sku The Stock Keeping Unit (SKU), i.e. a merchant-specific identifier for a product or service, or the product to which the offer refers.
+ */
