c04361d5c36fb70978c140dbbf2160150d0c14af
Chris Harvey <chrisharvey2pi@gmail.com>
2018-01-22 16:19:53 -0500

    add additionalProperty.prop , {Place}#additionalProperty

--- docs/src/Property.typedef.js
+++ docs/src/Property.typedef.js
@@ -1,4 +1,10 @@
 /**
+ * @summary A property-value pair representing an additional characteristics of the entitity, e.g. a product feature or another characteristic for which there is no matching property in schema.org.
+ * @see http://schema.org/additionalProperty
+ * @typedef {PropertyValue} additionalProperty
+ */
+
+/**
  * @summary An award won by or for this item.
  * @see http://schema.org/award
  * @typedef {(string|Array<string>)} award
