4bd78b0d9e6b10fc0b2115a05cfb44b8e55a0b65
Chris Harvey <chrisharvey2pi@gmail.com>
2018-01-23 18:44:48 -0500

    generate typedef.js from jsd files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const fs   = require('fs')
 const path = require('path')
+const url  = require('url')
 
 const gulp  = require('gulp')
 const jsdoc = require('gulp-jsdoc3')
@@ -19,9 +20,76 @@ gulp.task('test', function () {
 })
 
 
+gulp.task('docs:api:compile', function (callback) {
+  function jsDocType(jsonSchema) {
+    return 'boolean'
+  }
+
+  const datatype_names = [
+    'Date',
+    'DateTime',
+    'Time',
+    'URL',
+  ]
+
+  let contents = [
+    // datatypes
+    SCHEMATA.filter(function (jsd) {
+      let filename = path.parse(new url.URL(jsd['$id']).pathname).name
+      return datatype_names.includes(filename)
+    }).map(function (jsd) {
+      let name = path.parse(new url.URL(jsd.title).pathname).name
+      return `
+/**
+ * @summary ${jsd.description}
+ * @see ${jsd.title}
+ * @typedef {${jsDocType(jsd)}} ${name}
+ */
+      `
+    }).join(''),
+
+    // classes
+    SCHEMATA.filter(function (jsd) {
+      let filename = path.parse(new url.URL(jsd['$id']).pathname).name
+      return filename[0] === filename[0].toUpperCase() && !datatype_names.includes(filename)
+    }).map(function (jsd) {
+      let name = path.parse(new url.URL(jsd.title).pathname).name
+      let superclass = (name === 'Thing') ? `!Object` : path.parse(jsd.allOf[0]['$ref']).name
+      return `
+/**
+ * @summary ${jsd.description}
+ * @see ${jsd.title}
+ * @typedef {${superclass}} ${name}
+${Object.entries(jsd.allOf[1].properties).map((entry) =>
+  ` * @property {${jsDocType(entry[1])}=} ${entry[0]} ${entry[1].description}`).join('\n')}
+ */
+      `
+    }).join(''),
+
+    // properties
+    SCHEMATA.filter(function (jsd) {
+      let filename = path.parse(new url.URL(jsd['$id']).pathname).name
+      return filename[0] === filename[0].toLowerCase() && filename !== 'json-ld' // TODO: reference json-ld.jsd externally
+    }).map(function (jsd) {
+      let name = path.parse(new url.URL(jsd.title).pathname).name
+      return `
+/**
+ * @summary ${jsd.description}
+ * @see ${jsd.title}
+ * @typedef {${jsDocType(jsd)}} ${name}
+ */
+      `
+    }).join(''),
+  ].join('')
+
+  return fs.mkdir('./docs/build/', function (err) {
+    fs.writeFile('./docs/build/typedef.js', contents, 'utf8', callback) // send cb here to maintain dependency
+  })
+})
+
 // HOW-TO: https://github.com/mlucool/gulp-jsdoc3#usage
-gulp.task('docs:api', function () {
-  return gulp.src(['./README.md', './index.js', './docs/src/*.js'], {read:false})
+gulp.task('docs:api', ['docs:api:compile'], function () {
+  return gulp.src(['./README.md', './index.js', './docs/build/typedef.js'], {read:false})
     .pipe(jsdoc(require('./jsdoc.config.json')))
 })
 
