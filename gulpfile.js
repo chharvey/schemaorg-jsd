7bf6f481bf394899c73fe962b7fee00777f55415
Chris Harvey <chrisharvey2pi@gmail.com>
2018-01-24 12:49:47 -0500

    fix JSDoc type descriptions

--- gulpfile.js
+++ gulpfile.js
@@ -21,8 +21,25 @@ gulp.task('test', function () {
 
 
 gulp.task('docs:api:compile', function (callback) {
+
+  /**
+   * @summary Return the JSDoc-style type of a JSON Schema object.
+   * @param   {(!Object|boolean)} jsonSchema a JSON Schema object
+   * @returns {string} a JSDoc type description as a string, e.g., `'(boolean|number|string)'`
+   */
   function jsDocType(jsonSchema) {
+    if (jsonSchema.$ref) {
+      return jsonSchema.$ref.split('.')[0]
+    } else if (jsonSchema.allOf) {
+      return jsDocType(jsonSchema.allOf[0])
+    } else if (jsonSchema.anyOf) {
+      return `(${jsonSchema.anyOf.map(jsDocType).join('|')})`
+    } else if (jsonSchema.type) {
+      if (Array.isArray(jsonSchema.type)) return `(${jsonSchema.type.join('|')})`.replace('array','Array').replace('object','!Object')
+      else return jsonSchema.type.replace('array','Array').replace('object','!Object')
+    } else {
     return 'boolean'
+    }
   }
 
   const datatype_names = [
@@ -35,8 +52,8 @@ gulp.task('docs:api:compile', function (callback) {
   let contents = [
     // datatypes
     SCHEMATA.filter(function (jsd) {
-      let filename = path.parse(new url.URL(jsd['$id']).pathname).name
-      return datatype_names.includes(filename)
+      let name = path.parse(new url.URL(jsd['$id']).pathname).name
+      return datatype_names.includes(name)
     }).map(function (jsd) {
       let name = path.parse(new url.URL(jsd.title).pathname).name
       return `
@@ -50,26 +67,31 @@ gulp.task('docs:api:compile', function (callback) {
 
     // classes
     SCHEMATA.filter(function (jsd) {
-      let filename = path.parse(new url.URL(jsd['$id']).pathname).name
-      return filename[0] === filename[0].toUpperCase() && !datatype_names.includes(filename)
+      let name = path.parse(new url.URL(jsd['$id']).pathname).name
+      return name[0] === name[0].toUpperCase() && !datatype_names.includes(name)
     }).map(function (jsd) {
       let name = path.parse(new url.URL(jsd.title).pathname).name
       let superclass = (name === 'Thing') ? `!Object` : path.parse(jsd.allOf[0]['$ref']).name
+      let properties = Object.entries(jsd.allOf[1].properties).map(function (entry) {
+        // Try finding the `*.prop.jsd` file first, else use the `entry[1]` object.
+        let propSchema = SCHEMATA.find((jsd) => jsd.title===`http://schema.org/${entry[0]}`) || entry[1]
+        let type = jsDocType(propSchema)
+        return ` * @property {${type.replace(/#/g, name)}=} ${entry[0]} ${propSchema.description}`
+      })
       return `
 /**
  * @summary ${jsd.description}
  * @see ${jsd.title}
  * @typedef {${superclass}} ${name}
-${Object.entries(jsd.allOf[1].properties).map((entry) =>
-  ` * @property {${jsDocType(entry[1])}=} ${entry[0]} ${entry[1].description}`).join('\n')}
+${properties.join('\n')}
  */
       `
     }).join(''),
 
     // properties
     SCHEMATA.filter(function (jsd) {
-      let filename = path.parse(new url.URL(jsd['$id']).pathname).name
-      return filename[0] === filename[0].toLowerCase() && filename !== 'json-ld' // TODO: reference json-ld.jsd externally
+      let name = path.parse(new url.URL(jsd['$id']).pathname).name
+      return name[0] === name[0].toLowerCase() && name !== 'json-ld' // TODO: reference json-ld.jsd externally
     }).map(function (jsd) {
       let name = path.parse(new url.URL(jsd.title).pathname).name
       return `
