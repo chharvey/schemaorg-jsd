9e61bba7478c78452a9f2311146c3667d0a00e56
Chris Harvey <chrisharvey2pi@gmail.com>
2018-01-25 02:11:16 -0500

    export SCHEMA_TYPE

--- gulpfile.js
+++ gulpfile.js
@@ -22,41 +22,13 @@ gulp.task('test', function () {
 
 
 gulp.task('docs:api:compile', function (callback) {
-
-  /**
-   * @summary Return the JSDoc-style type of a JSON Schema object.
-   * @param   {(!Object|boolean)} jsonSchema a JSON Schema object
-   * @returns {string} a JSDoc type description as a string, e.g., `'(boolean|number|string)'`
-   */
-  function jsDocType(jsonSchema) {
-    if (jsonSchema.$ref) {
-      return jsonSchema.$ref.split('.')[0]
-    } else if (jsonSchema.allOf) {
-      return jsDocType(jsonSchema.allOf[0])
-    } else if (jsonSchema.anyOf) {
-      return `(${jsonSchema.anyOf.map(jsDocType).join('|')})`
-    } else if (jsonSchema.type) {
-      if (Array.isArray(jsonSchema.type)) return `(${jsonSchema.type.join('|')})`.replace('array','Array').replace('object','!Object')
-      else return jsonSchema.type.replace('array','Array').replace('object','!Object')
-    } else {
-    return 'boolean'
-    }
-  }
-
-  const schema_type = {
-    "http://json-schema.org/draft-07/schema#"                     : "DataType",
-    "https://chharvey.github.io/schemaorg-jsd/meta/type.jsd#"     : "Class",
-    "https://chharvey.github.io/schemaorg-jsd/meta/type-root.jsd#": "Class",
-    "https://chharvey.github.io/schemaorg-jsd/meta/member.jsd#"   : "Property",
-  }
-
   let contents = [
     { type: 'DataType', getter: 'jsdocDataDef'     },
     { type: 'Class'   , getter: 'jsdocTypeDef'     },
     { type: 'Property', getter: 'jsdocPropertyDef' },
   ].map((specs) => SCHEMATA
     .filter((jsd) => path.parse(new url.URL(jsd['$id']).pathname).name !== 'json-ld') // TODO: reference json-ld.jsd externally
-    .filter((jsd) => schema_type[jsd.$schema] === specs.type)
+    .filter((jsd) => JSONSchema.SCHEMA_TYPE[jsd.$schema] === specs.type)
     .map((jsd) => new JSONSchema(jsd)[specs.getter])
     .join('')
   ).join('')
