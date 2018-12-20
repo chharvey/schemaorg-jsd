a60bf9329bb96206742e6285027e714bb68b1af9
Chris Harvey <chrisharvey2pi@gmail.com>
2018-12-20 01:51:33 -0500

    convert package js to ts

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,48 @@
-const fs   = require('fs')
-const path = require('path')
-const url  = require('url')
-const util = require('util')
+import * as  fs   from 'fs'
+import * as  path from 'path'
+import * as  util from 'util'
 
-const Ajv = require('ajv')
+import * as Ajv from 'ajv'
 
-const { requireJSONLDAsync } = require('./lib/requireJSONLD.js')
+import { JSONSchemaObject } from '../lib/json-schema.d'
+import { JSONLDObject } from '../lib/json-ld.d'
+import { requireJSONLDAsync } from '../lib/requireJSONLD'
 
 
 /**
- * @module schemaorg-jsd
+ * An array of meta-schemata against which the content schemata validate.
+ *
+ * This is for internal use only. Users should not be expected to use these meta-schemata.
+ * @returns an array of meta-schemata
  */
-module.exports = {
-/**
- * @summary An array of meta-schemata against which the content schemata validate.
- * @description This is for internal use only. Users should not be expected to use these meta-schemata.
- * @returns {Array<!Object>} an array of meta-schemata
- */
-async getMetaSchemata() {
+export async function getMetaSchemata(): Promise<JSONSchemaObject[]> {
   return Promise.all(
     (await util.promisify(fs.readdir)(path.resolve(__dirname, './meta/')))
       .filter((filename) => path.parse(filename).ext === '.jsd')
-      .map((filename) => requireJSONLDAsync(path.join(__dirname, './meta/', filename)))
+      .map((filename) => requireJSONLDAsync(path.join(__dirname, './meta/', filename)) as Promise<JSONSchemaObject>)
   )
-},
+}
 
 /**
- * @summary An array of all JSON Schemata validating Schema.org vocabulary.
- * @description This array contains all Schema.org schemata in this project.
+ * An array of all JSON Schemata validating Schema.org vocabulary.
+ *
+ * This array contains all Schema.org schemata in this project.
  * That is, schemata against which your JSON-LD documents should validate.
- * @returns {Array<!Object>} an array of schemata
+ * @returns an array of schemata
  */
-async getSchemata() {
+export async function getSchemata(): Promise<JSONSchemaObject[]> {
   return Promise.all(
     (await util.promisify(fs.readdir)(path.resolve(__dirname, './schema/')))
       .filter((filename) => path.parse(filename).ext === '.jsd')
-      .map((filename) => requireJSONLDAsync(path.join(__dirname, './schema/', filename)))
+      .map((filename) => requireJSONLDAsync(path.join(__dirname, './schema/', filename)) as Promise<JSONSchemaObject>)
   )
-},
+}
 
 /**
- * @summary Validate a JSON-LD document against a Schema.org JSON schema.
- * @example
- * const {sdoValidate} = require('schemaorg-jsd')
+ * Validate a JSON-LD document against a Schema.org JSON schema.
+ *
+ * ```js
+ * const { sdoValidate } = require('schemaorg-jsd')
  * async function compile(jsdoc) {
  *   let is_valid;
  *   try {
@@ -58,36 +58,42 @@ async getSchemata() {
  *     .catch((e) => false)
  *     .then((result) => { console.log(result) })
  * }
+ * ```
  *
- * @param   {(!Object|string)} document the JSON or JSON-LD object to test, or its path pointing to a `.json` or `.jsonld` file
- * @param   {string=} type the name of the Type to test against; should be a Class in http://schema.org/
+ * @param   document the JSON or JSON-LD object to test, or its path pointing to a `.json` or `.jsonld` file
+ * @param   type the name of the Type to test against; should be a Class in http://schema.org/
  *                         ; see the API for supported Types
  *                         ; if omitted, will test against the JSON document’s `@type` property (if it has one)
  *                         ; if the `@type` is not supported or cannot be found, defaults to `'Thing'`
- * @returns {boolean} `true` if the document passes validation
+ * @returns `true` if the document passes validation
  * @throws  {TypeError} if the document fails validation; has a `.details` property for validation details
  */
-async sdoValidate(document, type = null) {
-  let doc = (typeof document === 'string') ? await requireJSONLDAsync(document) : document
-  const [META_SCHEMATA, SCHEMATA] = await Promise.all([module.exports.getMetaSchemata(), module.exports.getSchemata()])
+export async function sdoValidate(document: JSONLDObject|string, type: string|null = null): Promise<boolean> {
+	let doc: JSONLDObject = (typeof document === 'string') ? await requireJSONLDAsync(document) as JSONLDObject : document
+	const [META_SCHEMATA, SCHEMATA]: JSONSchemaObject[][] = await Promise.all([getMetaSchemata(), getSchemata()])
   if (type === null) {
-    let doctype = doc['@type']
-    if (SCHEMATA.find((jsd) => jsd.title === `http://schema.org/${doctype}`)) {
-      type = doctype
-    } else {
-      if (doctype) console.warn(`Class \`${doctype}\` is not yet supported. Validating against \`Thing.jsd\` instead.`)
-      else console.warn(`JSON-LD \`@type\` property was not found. Validating against \`Thing.jsd\`.`)
-      type = 'Thing'
-    }
+		let doctype: string[]|string|null = doc['@type'] || null
+		if (doctype instanceof Array && doctype.length) {
+			return (await Promise.all(doctype.map((dt) => sdoValidate(doc, dt)))).reduce((a, b) => a && b)
+		} else if (typeof doctype === 'string') {
+			type = (SCHEMATA.find((jsd) => jsd.title === `http://schema.org/${doctype}`)) ? doctype :
+				(console.warn(`Class \`${doctype}\` is not yet supported. Validating against \`Thing.jsd\` instead…`), 'Thing')
+		} else {
+			console.warn(`JSON-LD \`@type\` property was not found. Validating against \`Thing.jsd\`…`)
+			type = 'Thing'
+		}
   }
-  let ajv = new Ajv().addMetaSchema(META_SCHEMATA).addSchema(SCHEMATA)
-  let is_data_valid = ajv.validate(`https://chharvey.github.io/schemaorg-jsd/schema/${type}.jsd`, doc)
+
+	let ajv: Ajv.Ajv = new Ajv().addMetaSchema(META_SCHEMATA).addSchema(SCHEMATA)
+	let is_data_valid: boolean = ajv.validate(`https://chharvey.github.io/schemaorg-jsd/schema/${type}.jsd`, doc) as boolean
   if (!is_data_valid) {
-    let e = new TypeError(`Document ${doc['@id'] || doc.identifier || doc.name || doc} does not valiate against schema ${type}.jsd!`)
+		let e: TypeError&{
+			filename?: string;
+			details?: Ajv.ErrorObject;
+		} = new TypeError(`Document ${doc['@id'] || doc.identifier || doc.name || doc} does not valiate against schema ${type}.jsd!`)
     if (typeof document === 'string') e.filename = document
-    e.details = ajv.errors
+		e.details = ajv.errors ![0]
     throw e
   }
   return true
-},
 }
