75670ae0f9e7488364a18556aacc65016ed27669
Chris Harvey <chrisharvey2pi@gmail.com>
2019-01-12 00:48:13 -0500

    update metas && add d.ts

---
+++ src/meta-ld.d.ts
@@ -0,0 +1,70 @@
+import { JSONSchema7, JSONSchema7Version } from 'json-schema'
+
+import { JSONLDObject } from '@chharvey/requirejson'
+
+
+interface SDO_LD extends JSONLDObject {
+	'@id'          : string;
+	'rdfs:label'   : string;
+	'rdfs:comment' : string;
+}
+
+/**
+ * JSON-LD for a Schema.org Datatype.
+ */
+export interface SDODatatypeLD extends SDO_LD {
+	'@type': 'rdfs:Datatype';
+}
+
+/**
+ * JSON-LD for a Schema.org Class.
+ */
+export interface SDOClassLD extends SDO_LD {
+	'@type'           : 'rdfs:Class';
+	'rdfs:subClassOf' : SingleReferenceLD|null;
+	'rdfs:member'     : SingleReferenceLD[];
+	/**
+	 * Non-normative known subclasses of this class.
+	 *
+	 * Subclasses are non-normative because this information can be processed from each class’s superclass.
+	 */
+	superClassOf: SingleReferenceLD[];
+	/**
+	 * Non-normative known properties of which an instance of this class could appear as a value.
+	 *
+	 * A class’s `valueOf` is non-normative because this information can be processed from each property’s `rdfs:range`.
+	 */
+	valueOf: SingleReferenceLD[];
+}
+
+/**
+ * JSON-LD for a Schema.org Property.
+ */
+export interface SDOPropertyLD extends SDO_LD {
+	'@type'              : 'rdf:Property';
+	'rdfs:subPropertyOf' : SingleReferenceLD|null;
+	/**
+	 * Non-normative classes that own this property as a member.
+	 *
+	 * A property’s `rdfs:domain` is non-normative because this information can be processed from each type’s members.
+	 */
+	'rdfs:domain' : SingleReferenceLD[];
+	'rdfs:range'  : SingleReferenceLD[];
+	/**
+	 * Non-normative known subproperties of this property.
+	 *
+	 * Subproperties are non-normative because this information can be processed from each property’s superproperty.
+	 */
+	superPropertyOf: SingleReferenceLD[],
+	/**
+	 * Non-standard: whether this property accepts multiple values.
+	 */
+	$rangeArray: boolean,
+}
+
+/**
+ * A single reference to a JSON-LD object.
+ */
+export type SingleReferenceLD = {
+	'@id': string;
+}
---
+++ src/meta-schemata.d.ts
@@ -0,0 +1,142 @@
+import { JSONSchema7, JSONSchema7Version } from 'json-schema'
+
+
+/**
+ * JSON Schema for a Schema.org Datatype.
+ */
+export interface SDODatatypeSchema extends JSONSchema7 {
+	/**
+	 * @override JSONSchema7
+	 */
+	$schema: JSONSchema7Version;
+	/**
+	 * @override JSONSchema7
+	 * @format uri
+	 */
+	$id: string;
+	/**
+	 * The canonical URL of the Schema.org Datatype.
+	 * @override JSONSchema7
+	 * @format uri
+	 */
+	title: string;
+	/**
+	 * Schema.org’s canonical description of the Datatype.
+	 * @override JSONSchema7
+	 */
+	description: string;
+	/**
+	 * @override JSONSchema7
+	 */
+	type: 'string'|'number'|'integer'|'boolean'|'null';
+}
+
+/**
+ * JSON Schema for a Schema.org Class instance in JSON-LD.
+ * @id https://chharvey.github.io/schemaorg-jsd/meta/type.jsd
+ */
+export interface SDOClassSchema extends JSONSchema7 {
+	/**
+	 * @override JSONSchema7
+	 */
+	$schema: 'https://chharvey.github.io/schemaorg-jsd/meta/type.jsd#';
+	/**
+	 * @override JSONSchema7
+	 * @format uri
+	 */
+	$id: string;
+	/**
+	 * The canonical URL of the Schema.org Class.
+	 * @override JSONSchema7
+	 * @format uri
+	 */
+	title: string;
+	/**
+	 * Schema.org’s canonical description of the Class.
+	 * @override JSONSchema7
+	 */
+	description: string;
+	/**
+	 * @override JSONSchema7
+	 * @minItems 2
+	 * @maxItems 2
+	 */
+	allOf: [
+		/** The superclass of this class. */
+		SingleReferenceSchema,
+		/** An object having further properties of this class. */
+		{
+			type?: 'object',
+			properties: {
+				/** A reference to an external JSON schema for a Schema.org Property. */
+				[key: string]: SingleReferenceSchema;
+			}
+		}
+	];
+}
+
+/**
+ * JSON Schema for a Schema.org Property instance in JSON-LD.
+ * @id https://chharvey.github.io/schemaorg-jsd/meta/member.jsd
+ */
+export interface SDOPropertySchema extends JSONSchema7 {
+	/**
+	 * @override JSONSchema7
+	 */
+	$schema: 'https://chharvey.github.io/schemaorg-jsd/meta/member.jsd#';
+	/**
+	 * @override JSONSchema7
+	 * @format uri
+	 */
+	$id: string;
+	/**
+	 * The canonical URL of the Schema.org Property.
+	 * @override JSONSchema7
+	 * @format uri
+	 */
+	title: string;
+	/**
+	 * Schema.org’s canonical description of the Property.
+	 * @override JSONSchema7
+	 */
+	description: string;
+	/**
+	 * @override JSONSchema7
+	 * @minItems 2
+	 * @maxItems 2
+	 */
+	allOf: [
+		/** The superproperty of this property, or: There is no superproperty of this property. */
+		SingleReferenceSchema|true,
+		/** References to the expected type(s) of this property. */
+		{
+			/**
+			 * @override JSONSchema7
+			 * @maxItems: 2
+			 */
+			anyOf: [
+				{ $ref: '#/definitions/ExpectedType' }
+			]|[
+				{ $ref: '#/definitions/ExpectedType' },
+				{ type: 'array', items: { $ref: '#/definitions/ExpectedType' } }
+			];
+		}
+	];
+	definitions: {
+		/** The range of this property. */
+		ExpectedType: {
+			/** References to external JSON schema for Schema.org Classes. */
+			anyOf: SingleReferenceSchema[];
+		};
+	};
+}
+
+/**
+ * A single reference to a JSON schema.
+ */
+export type SingleReferenceSchema = {
+	/**
+	 * @format uri
+	 */
+	$ref: string;
+}
