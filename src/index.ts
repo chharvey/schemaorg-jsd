bed47cf3af7f5faa5fee6a136df5e57a4ace3ce4
Chris Harvey <chrisharvey2pi@gmail.com>
2019-02-08 12:49:32 -0500

    update code formatting & comments

--- src/index.ts
+++ src/index.ts
@@ -16,12 +16,10 @@ import { SDODatatypeSchema, SDOClassSchema, SDOPropertySchema } from './meta-sch
  *
  * This is for internal use only. Users should not be expected to use these meta-schemata.
  */
-export const META_SCHEMATA: Promise<JSONSchema7[]> = (async () => {
-  return Promise.all(
-    (await util.promisify(fs.readdir)(path.resolve(__dirname, '../meta/')))
-      .map((filename) => requireJSON(path.join(__dirname, '../meta/', filename)) as Promise<JSONSchema7>)
-  )
-})()
+export const META_SCHEMATA: Promise<JSONSchema7[]> = (async () => Promise.all(
+	(await util.promisify(fs.readdir)(path.resolve(__dirname, '../meta/')))
+		.map((filename) => requireJSON(path.join(__dirname, '../meta/', filename)) as Promise<JSONSchema7>)
+))()
 
 /**
  * A single JSON Schema, which validates JSON-LD objects.
@@ -29,6 +27,7 @@ export const META_SCHEMATA: Promise<JSONSchema7[]> = (async () => {
  */
 export const JSONLD_SCHEMA: Promise<JSONSchema7> = new Promise((resolve, reject) => {
 	https.get('https://cdn.jsdelivr.net/gh/json-ld/json-ld.org@1.0/schemas/jsonld-schema.json', (res) => {
+		// if failing to load page, reject
 		if (!res.statusCode || res.statusCode < 200 || 300 <= res.statusCode) {
 			reject(new Error(`
 Failed to load.
@@ -42,17 +41,18 @@ Status Code: ${res.statusCode || 'no status code found'}
 		res.on('data', (chunk) => { body.push(chunk) })
 		res.on('end', () => {
 			let data: JSONSchema4;
+			// if failing to parse contents as json, reject
 			try {
 				data = JSON.parse(body.join(''))
-			} catch (e) {
-				reject(e)
+			} catch (err) {
+				reject(err)
 				return;
 			}
 			data.$schema = 'http://json-schema.org/draft-07/schema#'
 			data.$id = 'https://json-ld.org/schemas/jsonld-schema.json'
 			resolve(data as JSONSchema7)
 		})
-	}).on('error', (e) => { reject(e) })
+	}).on('error', (err) => { reject(err) }) // if failing to get url, reject
 })
 
 /**
@@ -61,12 +61,10 @@ Status Code: ${res.statusCode || 'no status code found'}
  * This array contains all Schema.org schemata in this project.
  * That is, schemata against which your JSON-LD documents should validate.
  */
-export const SCHEMATA: Promise<(SDODatatypeSchema|SDOClassSchema|SDOPropertySchema)[]> = (async () => {
-  return Promise.all(
-    (await util.promisify(fs.readdir)(path.resolve(__dirname, '../schema/')))
-      .map((filename) => requireJSON(path.join(__dirname, '../schema/', filename)) as Promise<JSONSchema7> as Promise<(SDODatatypeSchema|SDOClassSchema|SDOPropertySchema)>)
-  )
-})()
+export const SCHEMATA: Promise<(SDODatatypeSchema|SDOClassSchema|SDOPropertySchema)[]> = (async () => Promise.all(
+	(await util.promisify(fs.readdir)(path.resolve(__dirname, '../schema/')))
+		.map((filename) => requireJSON(path.join(__dirname, '../schema/', filename)) as Promise<JSONSchema7> as Promise<(SDODatatypeSchema|SDOClassSchema|SDOPropertySchema)>)
+))()
 
 /**
  * Validate a JSON-LD object against a Schema.org JSON schema.
@@ -77,7 +75,7 @@ export const SCHEMATA: Promise<(SDODatatypeSchema|SDOClassSchema|SDOPropertySche
  * 	let is_valid;
  * 	try {
  * 		is_valid = await sdoValidate(ldobj)
- * 	} catch (e) {
+ * 	} catch (err) {
  * 		is_valid = false
  * 	}
  * 	console.log(is_valid)
@@ -85,7 +83,7 @@ export const SCHEMATA: Promise<(SDODatatypeSchema|SDOClassSchema|SDOPropertySche
  * // or you could use its Promise (if `async`/`await` is not supported):
  * function compilePromise(ldobj) {
  * 	sdoValidate(ldobj)
- * 		.catch((e) => false)
+ * 		.catch((err) => false)
  * 		.then((result) => { console.log(result) })
  * }
  * ```
@@ -108,7 +106,7 @@ export async function sdoValidate(obj: JSONLDObject|string, type: string|null = 
 	if (type === null) {
 		let objtype: string[]|string|null = obj['@type'] || null
 		if (objtype instanceof Array && objtype.length) {
-			return (await Promise.all(objtype.map((dt) => sdoValidate(obj, dt)))).reduce((a, b) => a && b)
+			return (await Promise.all(objtype.map((tp) => sdoValidate(obj, tp)))).every((a) => !!a) as true
 		} else if (typeof objtype === 'string') {
 			type = ((await SCHEMATA).find((jsd) => jsd.title === `http://schema.org/${objtype}`)) ? objtype :
 				(console.warn(`Class \`${objtype}\` is not yet supported. Validating against \`Thing.jsd\` instead…`), 'Thing')
@@ -124,13 +122,13 @@ export async function sdoValidate(obj: JSONLDObject|string, type: string|null = 
 		.addSchema(await SCHEMATA)
 	let is_data_valid: boolean = ajv.validate(`https://chharvey.github.io/schemaorg-jsd/schema/${type}.jsd`, obj) as boolean
 	if (!is_data_valid) {
-		let e: TypeError&{
+		let err: TypeError&{
 			filename?: string;
 			details?: Ajv.ErrorObject;
 		} = new TypeError(`Object ${obj['@id'] || obj.identifier || obj.name || JSON.stringify(obj)} does not valiate against schema ${type}.jsd!`)
-		if (filename.length) e.filename = filename
-		e.details = ajv.errors ![0]
-		throw e
+		if (filename.length) err.filename = filename
+		err.details = ajv.errors ![0]
+		throw err
 	}
 	return true
 }
